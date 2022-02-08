e32ea22a1bdbe7de37a125280ce91147286faf28
victor <victor.robin@epsi.fr>
2022-02-09 00:16:30 +0100

    base du site

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,28 @@
 /* ::::::::::::::::::::::::::::::::::::::::::::::: -- Nav Bar -- ::::::::::::::::::::::::::::::::::::::::::::::: */
 var triggered = false;
-$('.navTrigger').click(function () {
+$('.navTrigger').click(function() {
     $(this).toggleClass('active');
-    if(triggered == false){
+    if (triggered == false) {
         $("#mainListDiv").addClass("show_list");
         $("#mainListDiv").fadeIn(500);
         triggered = true;
-    }
-    else{
-        $("#mainListDiv").fadeOut(500, function () {
+    } else {
+        $("#mainListDiv").fadeOut(500, function() {
             $("#mainListDiv").removeClass("show_list");
             triggered = false;
         });
+    }
+});
+var triggered_menu = false;
+
+$('.menu').click(function() {
+    if (triggered_menu == false) {
+        $("#navRight").removeClass("navRightHide");
+        $("#navRight").addClass("navRightShow");
+        triggered_menu = true;
+    } else {
+        $("#navRight").removeClass("navRightShow");
+        $("#navRight").addClass("navRightHide");
+        triggered_menu = false;
     }
 });
